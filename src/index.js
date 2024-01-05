236b84da91366fe49002b763f131ab94c4ce2101
Pablo Calderon Bravo <pablocalderonbravo@gmail.com>
2024-01-04 20:29:09 -0600

    upgrading to v18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 
-/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -8,8 +7,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
-*/
 
+/*
+
+// version 17
 import React from 'react';
 // import ReactDOM from 'react-dom/client'; <- This import is only // for React version 18
 import { render } from 'react-dom'; // <- This is the correct import // statement for React version 17
@@ -20,3 +21,5 @@ render(
   <React.StrictMode>
     <App />
   </React.StrictMode>, root);
+
+*/
